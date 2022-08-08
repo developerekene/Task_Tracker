ee77c7bc26ef08bf71238ab6efb8bd311eb69a58
developerekene <developerekene@gmail.com>
2022-08-08 19:15:54 +0100

    Initial Set up

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text, StyleSheet, SafeAreaView} from 'react-native'
+
+export default function App(){
+  return(
+    <SafeAreaView>
+      <Text>
+        This is the main App Container
+      </Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+
+})
